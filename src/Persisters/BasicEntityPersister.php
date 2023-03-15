c86946d50e86db7728419300602ad72af54ea7cf
Jesus <jesus.donoso@sngular.com>
2023-03-15 08:34:05 +0100

    add into array

--- src/Persisters/BasicEntityPersister.php
+++ src/Persisters/BasicEntityPersister.php
@@ -291,7 +291,7 @@ class BasicEntityPersister
         $relPattern = sprintf('%s-[:`%s`]-%s', $isIncoming, $relationshipType, $isOutgoing);
 
         $cypher  = 'MATCH (n) WHERE id(n) = $id ';
-        $cypher .= 'RETURN size(p=(n)'.$relPattern.'('.$targetClassLabel.') | p) ';
+        $cypher .= 'RETURN size([p=(n)'.$relPattern.'('.$targetClassLabel.') | p]) ';
         $cypher .= 'AS '.$alias;
 
         return Statement::create($cypher, ['id' => $sourceEntityId]);
